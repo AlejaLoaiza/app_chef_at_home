fae62354e2691cef28053a106c638fc98c1624c8
Hilary Raigoso <dahianraigoso@gmail.com>
2025-03-09 00:40:39 -0500

    Adelanto clientes y galeria
    Co-authored-by: AlejaLoaiza <AlejaLoaiza@users.noreply.github.com>

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ function toggleMenu() {
     const menu = document.getElementById('menu');
     menu.classList.toggle('menu-visible'); 
 }
+
+let slideIndex = 0;
+mostrarSlide(slideIndex);
+
+function cambiarSlide(n) {
+    mostrarSlide(slideIndex += n);
+}
+
+function mostrarSlide(n) {
+    const testimonio = document.getElementsByClassName("testimonio");
+    if (n >= testimonio.length) { slideIndex = 0; }
+    if (n < 0) { slideIndex = testimonio.length - 1; }
+    
+    for (let i = 0; i < testimonio.length; i++) {
+        testimonio[i].classList.remove("active");
+    }
+    
+    testimonio[slideIndex].classList.add("active");
+}
